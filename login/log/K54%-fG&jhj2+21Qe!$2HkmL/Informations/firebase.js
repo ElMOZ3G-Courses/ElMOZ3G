3f7005b52dbfc9382683cb97a0eebad98b14eab1
ElMOZ3G-Courses <momayallo90@gmail.com>
2025-01-20 22:05:35 +0200

    Add files via upload

--- login/log/K54%-fG&jhj2+21Qe!$2HkmL/Informations/firebase.js
+++ login/log/K54%-fG&jhj2+21Qe!$2HkmL/Informations/firebase.js
@@ -89,3 +89,14 @@ login1.onclick = function(event2){
         });
     },4000)
 }
+const user = auth.currentUser;
+
+if (user !== null) {
+  user.providerData.forEach((profile) => {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });
+}
